b4a7bf776e3c9d3ad01da93c7d57da2ce6a25893
Plouf <Plouf@Lagny-GrosPC>
2016-10-05 18:25:24 +0200

    unit test integration

---
+++ test_sig.py
@@ -0,0 +1,29 @@
+'''
+Created on 5 oct. 2016
+
+@author: Plouf
+'''
+import unittest
+from sig import ComputeSig
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def test_sig0(self):
+        self.assertEqual(ComputeSig(8644, 79791104), 41221)
+    
+    def test_sig1(self):
+        self.assertEqual(ComputeSig(8708, 79787520), 20677)
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
